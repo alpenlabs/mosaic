9a13c29b4ff5db277079dad7b5e7519fd85b05e6
nakkstar123 <nakulkhambhati@g.ucla.edu>
2025-11-24 12:38:34 -0800

    re-export public API, hide internal module structure

--- crates/vs3/src/lib.rs
+++ crates/vs3/src/lib.rs
@@ -1,7 +1,22 @@
 //! Verifiable secret sharing over the secp256k1 curve.
 
-pub mod constants;
-pub mod error;
-pub mod interpolate;
-pub mod polynomial;
-pub mod psm;
+mod constants;
+mod error;
+mod interpolate;
+mod polynomial;
+mod psm;
+
+// Re-export all constants
+pub use constants::*;
+
+// Re-export error types
+pub use error::Error;
+
+// Re-export interpolation function
+pub use interpolate::interpolate;
+
+// Re-export polynomial types
+pub use polynomial::{Index, Polynomial, PolynomialCommitment, Share, ShareCommitment};
+
+// Re-export point scalar multiplication functions
+pub use psm::{gen_batch_mul, gen_mul, precomp};
