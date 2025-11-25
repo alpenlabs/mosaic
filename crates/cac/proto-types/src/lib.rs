3d3d0a6fb490b03b3b7301c67878ebd3d9e0adcd
Trey Del Bonis <trey@alpenlabs.io>
2025-11-25 12:36:00 -0500

    doc: update cac types crates docs

--- crates/cac/proto-types/src/lib.rs
+++ crates/cac/proto-types/src/lib.rs
@@ -1,3 +1,10 @@
+//! CaC protocol type definitions.
+//!
+//! This is for types that the bridge client might end up using, so we want to
+//! expose in the RPC interface.  These are defined separately from the main CaC
+//! types crate because those are more internally-focused that the bridge client
+//! won't be expected to use.
+
 #![allow(
     missing_docs,
     reason = "these as standins that we'll define properly at a future time"
--- crates/cac/types/src/lib.rs
+++ crates/cac/types/src/lib.rs
@@ -1,1 +1,5 @@
 //! CaC protocol type definitions.
+//!
+//! This is for internally-focused types that we don't expect the bridge client
+//! to ever interact with directly, so this can have types that we don't want to
+//! expose in the RPC interface (or be compiled into RPC libraries).
