762a390f02f5d40941a8472642cacd7512ca6707
Sapin Bajracharya <sapin@alpenlabs.io>
2026-04-02 19:56:13 +0545

    fix: secp version mismatch

--- crates/rpc/service/src/schnorr_signer.rs
+++ crates/rpc/service/src/schnorr_signer.rs
@@ -1,10 +1,11 @@
 use ark_ff::{BigInteger, PrimeField};
 use bitcoin::{
     TapNodeHash,
+    hashes::Hash as _,
     key::{Keypair, TapTweak},
 };
 use mosaic_vs3::Scalar;
-use secp256k1::{Message, SECP256K1, hashes::Hash, schnorr::Signature};
+use secp256k1::{Message, SECP256K1, schnorr::Signature};
 
 pub(crate) struct SchnorrSigner(Keypair);
 
@@ -20,11 +21,11 @@ impl SchnorrSigner {
     }
 
     pub(crate) fn sign(self, digest: [u8; 32], tweak: Option<[u8; 32]>) -> Signature {
-        let tweak = tweak.map(|h| TapNodeHash::from_slice(&h).expect("guaranteed correct length"));
+        let tweak = tweak.map(TapNodeHash::from_byte_array);
 
         self.0
             .tap_tweak(SECP256K1, tweak)
             .to_keypair()
-            .sign_schnorr(Message::from_digest_slice(&digest).expect("digest is 32 bytes"))
+            .sign_schnorr(Message::from_digest(digest))
     }
 }
